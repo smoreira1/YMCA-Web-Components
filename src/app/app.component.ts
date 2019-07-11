cbbc96df767267eb36fd7139466dd97450edbaf5
Stephen Moreira <smoreira1@live.com>
2019-07-11 13:05:21 -0400

    Added Material Theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ebiz-elements';
+
+    ngOnInit():void{
+      const eventCard = document.createElement('app-ymca-event-card');
+      const content = document.getElementById('custom-elements');
+      content.appendChild(eventCard);
+
+    }
+
 }
